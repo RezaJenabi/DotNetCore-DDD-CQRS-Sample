90ffa9a4d02bff56db1a3390388c22d6ae05bd6d
pc2 <pc2@DESKTOP-0JP2S3D>
2019-10-12 12:08:22 +0330

    test framework

--- Domain/Models/Customers/Customer.cs
+++ Domain/Models/Customers/Customer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Core.BaseEntities;
 using Domain.Models.Orders;
+using Domain.Models.Payments;
 
 namespace Domain.Models.Customers
 {
@@ -13,25 +14,19 @@ namespace Domain.Models.Customers
         public string LastName { get; protected set; }
         public string Email { get; protected set; }
         public ReadOnlyCollection<Order> Orders => this._orders.AsReadOnly();
+        public void Checkout(Order order, Payment payment)
+        {
+            var orderItem = new Order(this, payment);
+            _orders.Add(orderItem);
+        }
 
-        //public Purchase Checkout(Cart cart)
+        //public Purchase Checkout(Order order) 
         //{
         //    Purchase purchase = Purchase.Create(this, cart.Products);
         //    this.purchases.Add(purchase);
         //    DomainEvents.Raise(new CustomerCheckedOut() { Purchase = purchase });
         //    return purchase;
         //}
-
-        public static Customer Create(string firstName, string lastName, string email)
-        {
-            var customer = new Customer()
-            {
-                FirstName = firstName,
-                LastName = lastName,
-                Email = email
-            };
-            return customer;
-        }
 
         public DateTime CreatedAt { get; set; }
         public string CreatedBy { get; set; }
@@ -40,5 +35,34 @@ namespace Domain.Models.Customers
         public bool Deleted { get; set; }
         public DateTime DeletedAt { get; set; }
         public string DeletedBy { get; set; }
+
+        public static Customer Create(string firstname, string lastname, string email)
+        {
+            return Create(Guid.NewGuid(), firstname, lastname, email);
+        }
+
+        public static Customer Create(Guid id, string firstname, string lastname, string email)
+        {
+            if (string.IsNullOrEmpty(firstname))
+                throw new ArgumentNullException(nameof(firstname));
+
+            if (string.IsNullOrEmpty(lastname))
+                throw new ArgumentNullException(nameof(lastname));
+
+            if (string.IsNullOrEmpty(email))
+                throw new ArgumentNullException(nameof(email));
+
+
+
+            var customer = new Customer()
+            {
+                Id = id,
+                FirstName = firstname,
+                LastName = lastname,
+                Email = email
+            };
+
+            return customer;
+        }
     }
 }
--- Domain/Models/Orders/OrderDetail.cs
+++ Domain/Models/Orders/OrderDetail.cs
@@ -1,8 +1,13 @@
-﻿namespace Domain.Models.Orders
+﻿using Domain.Models.Products;
+
+namespace Domain.Models.Orders
 {
     public class OrderDetail
     {
         public Order Order { get;protected set; }
+        public Product Product { get; protected set; }
+        public int Some { get; protected set; }
+        public double Amount { get; protected set; }
 
     }
 }
